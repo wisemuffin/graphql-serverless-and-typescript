467dc0f8531839c05ba8678fc632019ed3857ff1
David Griffiths <davidgg777@hotmail.com>
2020-08-19 18:25:21 +1000

    added seeding to db

---
+++ src/seed-data/insert_seed_data_prod.ts
@@ -0,0 +1,39 @@
+// Load the AWS SDK for Node.js
+const AWS = require("aws-sdk");
+const fs = require("fs");
+
+const tableName = process.env.tableName;
+import Dynamo from "../util/Dynamo";
+
+console.log("Importing data into DynamoDB. Please wait.");
+
+const allUsers = JSON.parse(fs.readFileSync("Users.json", "utf8"));
+const allTweets = JSON.parse(fs.readFileSync("Tweets.json", "utf8"));
+
+allUsers.forEach(function (user) {
+  const Userparams = {
+    name: user.name,
+    handle: user.handle,
+    location: user.location,
+    description: user.description,
+    followers_count: user.followers_count,
+    friends_count: user.friends_count,
+    favourites_count: user.favourites_count,
+    following: user.following,
+  };
+
+  Dynamo.write(Userparams, "userTable");
+});
+
+allTweets.forEach(function (tweet) {
+  const Tweetparams = {
+    handle: tweet.handle,
+    tweet_id: tweet.tweet_id,
+    tweet: tweet.tweet,
+    retweeted: tweet.retweeted,
+    retweet_count: tweet.retweet_count,
+    favorited: tweet.favorited,
+    created_at: tweet.created_at,
+  };
+  Dynamo.write(Tweetparams, "tweetsTable");
+});
